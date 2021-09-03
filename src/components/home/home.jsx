32b41bba3e06083c7ae208853676fe37d86ab685
SantiguzmaN <santiagoguzmanarango@gmail.com>
2021-09-02 23:56:54 -0500

    Creacion del formulario de registro

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+
+  const [serialEquipo, setSerialEquipo] = useState('');
+  const [nombreEquipo, setNombreEquipo] = useState('');
+  const [tipo, setTipo] = useState('');
+  const [sistemaOperativo, setSistemaOperativo] = useState('');
+  const [nombrePropietario, setNombrePropietario] = useState('');
+  const [mailPropietario, setMailPropietario] = useState('');
+  const [fechaAsignacion, setFechaAsignacion] = useState('');
+  const [hiddenItems, setBiddenItems] = useState(true);
+
+
+
   const handleEditSubmit = (e) => {
     e.preventDefault();
-    //aca va el metodo para el submit
   };
 
   return (
@@ -16,16 +27,113 @@ const Home = () => {
             onSubmit={handleEditSubmit}
           >
             <h2 className="w-100">
-              validacion de equipo
+              Registro de dotacion
             </h2>
-            <input
-              type="input"
-              className="form-control"
-              name="input"
-              placeholder="Input"
-            />
-            <button className="btn btn-info btn-block my-2" type="submit" data-testid="submit">
-              Validar
+            <div className="row">
+              <div className="col-3">
+                <p className="mt-1">Serial del equipo</p>
+              </div>
+              <div className="col-9">
+                <input
+                  type="input"
+                  className="form-control mb-1"
+                  name="idForm"
+                  placeholder="Serial del equipo"
+                  value={serialEquipo}
+                  onChange={e => setSerialEquipo(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="row" hidden={hiddenItems}>
+              <div className="col-3">
+                <p className="mt-1">Nombre</p>
+              </div>
+              <div className="col-9">
+                <input
+                  type="input"
+                  className="form-control mb-1"
+                  name="nombre"
+                  placeholder="ingrese el nombre"
+                  value={nombreEquipo}
+                  onChange={e => setNombreEquipo(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="row" hidden={hiddenItems}>
+              <div className="col-2">
+                <p className="mt-1">Tipo</p>
+              </div>
+              <div className="col-4">
+                <input
+                  type="input"
+                  className="form-control mb-1"
+                  name="nombre"
+                  placeholder="ingrese el tipo"
+                  value={tipo}
+                  onChange={e => setTipo(e.target.value)}
+                />
+              </div>
+              <div className="col-3">
+                <p className="mt-1">Sistema operativo</p>
+              </div>
+              <div className="col-3">
+                <input
+                  type="input"
+                  className="form-control mb-1"
+                  name="so"
+                  placeholder="ingrese el S.O"
+                  value={sistemaOperativo}
+                  onChange={e => setSistemaOperativo(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="row" hidden={hiddenItems}>
+              <div className="col-3">
+                <p className="mt-1">Nombre (propietario)</p>
+              </div>
+              <div className="col-9">
+                <input
+                  type="input"
+                  className="form-control mb-1"
+                  name="nombrePropietario"
+                  placeholder="Nombre del propietario"
+                  value={nombrePropietario}
+                  onChange={e => setNombrePropietario(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="row" hidden={hiddenItems}>
+              <div className="col-3">
+                <p className="mt-1">Email (propietario)</p>
+              </div>
+              <div className="col-9">
+                <input
+                  type="input"
+                  className="form-control mb-1"
+                  name="mailPropietario"
+                  placeholder="Email del propietario"
+                  value={mailPropietario}
+                  onChange={e => setMailPropietario(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="row" hidden={hiddenItems}>
+              <div className="col-3">
+                <p className="mt-1">Fecha de Asignacion</p>
+              </div>
+              <div className="col-9">
+                <input
+                  type="date"
+                  className="form-control mb-1"
+                  name="fechaAsignacion"
+                  placeholder="Fecha de asignacion"
+                  value={fechaAsignacion}
+                  onChange={e => setFechaAsignacion(e.target.value)}
+                />
+              </div>
+            </div>
+            <button className="btn btn-info btn-block my-2" type="submit" data-testid="submit" hidden={hiddenItems}>
+              Registrar
             </button>
           </form>
         </div>
